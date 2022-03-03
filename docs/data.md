966fd279f4c7a8724eaadada63e28e693bd3f4ee
ataola <zjt613@gmail.com>
2022-03-03 15:44:39 +0800

    ✨ feat: cloneDeep

--- docs/data.md
+++ docs/data.md
@@ -9,3 +9,11 @@ data.deepFreeze(obj);
 obj.b = 1;
 console.log(obj.b) // undefined
 ```
+
+## cloneDeep(obj)
+
+```javascript
+const arr = [{ x: 1 }, { x: 2 }];
+const newArr = cloneDeep(arr);
+newArr[0].x = 100;
+```
--- test/data.test.js
+++ test/data.test.js
@@ -1,11 +1,19 @@
 import { expect } from 'chai';
 
-import { deepFreeZe } from '../lib/data';
+import { deepFreeZe, cloneDeep } from '../lib/data';
 
 describe('lib: data test', function () {
   it('deepFreeze: obj.internal is not extensible', function () {
     let obj = { internal: {} };
     obj = deepFreeZe(obj);
     expect(Object.isExtensible(obj.internal)).to.be.false;
+  });
+
+  it('cloneDeep', function () {
+    const arr = [{ x: 1 }, { x: 2 }];
+    const newArr = cloneDeep(arr);
+    newArr[0].x = 100;
+    expect(arr[0].x).to.equal(1);
+    expect(newArr[0].x).to.equal(100);
   });
 });
